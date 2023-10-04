ab59c4adbc3e6bd81112c284f7a5aac13f59e14d
Andrea Bezzolato <bezz0001@hz.nl>
2023-10-04 18:08:58 +0200

    Last changes

--- sidebarScript.js
+++ sidebarScript.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("movable").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "350px";
-    document.getElementById("closeBtn").style.visibility="visible";
-  }
-  
-  function closeNav() {
-    document.getElementById("movable").style.width = "0";
-    document.getElementById("main").style.marginLeft= "150px";
-    document.getElementById("closeBtn").style.visibility="hidden";
-  }
+  document.getElementById("movable").style.width = "200px";
+  document.getElementById("main").style.marginLeft = "350px";
+  document.getElementById("closeBtn").style.visibility = "visible";
+}
+
+function closeNav() {
+  document.getElementById("movable").style.width = "0";
+  document.getElementById("main").style.marginLeft = "200px";
+  document.getElementById("closeBtn").style.visibility = "hidden";
+}
